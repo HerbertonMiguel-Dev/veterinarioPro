7c83abd03ad6befb281a020648b1886bc8b3abab
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-10-29 09:34:52 -0300

    adicionado rota de autenticação de usuário

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Router, Request, Response } from 'express'
+
 import { CreateUserController } from './controllers/usuario/CreateUserController';
+import { AuthUserController } from './controllers/usuario/AuthUserController'
 
 const router = Router();
 
 // --- ROTAS USER ---
 router.post('/usuarios', new CreateUserController().handle.bind(new CreateUserController()));
-
+router.post('/sessao', new AuthUserController().handle)
 
 export { router };
