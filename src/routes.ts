a7b4324a4aedd922eb961c041fdba1986444a913
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-10-29 09:53:02 -0300

    adicionado rota para detalhar dados do usuário autenticado

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,13 @@ import { Router, Request, Response } from 'express'
 
 import { CreateUserController } from './controllers/usuario/CreateUserController';
 import { AuthUserController } from './controllers/usuario/AuthUserController'
+import { DetailUserController } from './controllers/usuario/DetailUserController'
 
 const router = Router();
 
 // --- ROTAS USER ---
 router.post('/usuarios', new CreateUserController().handle.bind(new CreateUserController()));
 router.post('/sessao', new AuthUserController().handle)
+router.get('/me', new DetailUserController().handle)
 
 export { router };
