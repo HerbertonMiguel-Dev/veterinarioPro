ddaf5daa3080738977b65f77b1457e79dfdddf6a
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-10-29 10:21:19 -0300

    proteção da rota de detalhamento de usuário com middleware de autenticação

--- src/routes.ts
+++ src/routes.ts
@@ -4,11 +4,13 @@ import { CreateUserController } from './controllers/usuario/CreateUserController
 import { AuthUserController } from './controllers/usuario/AuthUserController'
 import { DetailUserController } from './controllers/usuario/DetailUserController'
 
+import { isAuthenticated } from './middlewares/isAuthenticated'
+
 const router = Router();
 
 // --- ROTAS USER ---
 router.post('/usuarios', new CreateUserController().handle.bind(new CreateUserController()));
 router.post('/sessao', new AuthUserController().handle)
-router.get('/me', new DetailUserController().handle)
+router.get('/me', isAuthenticated, new DetailUserController().handle)
 
 export { router };
