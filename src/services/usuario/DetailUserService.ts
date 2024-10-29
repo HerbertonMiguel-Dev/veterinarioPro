180926ae0c809aaeec10d9bfb4d61b2bc5f7f501
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-10-29 11:32:57 -0300

    implementado serviço para buscar detalhes do usuário no banco de dados

--- src/services/usuario/DetailUserService.ts
+++ src/services/usuario/DetailUserService.ts
@@ -1,8 +1,29 @@
 import prismaClient from "../../prisma";
 
 class ServicoDetalheUsuario{
-  async execute(){
-    return { ok: true }
+  async execute(usuario_id: string){
+
+    const usuario = await prismaClient.usuario.findFirst({
+      where:{
+        id: usuario_id
+      },
+      select:{
+        id: true,
+        nome: true,
+        email: true,
+        endereco: true,
+        assinaturas:{
+          select:{
+            id: true,
+            precoId: true,
+            status: true,
+          }
+        }
+      }
+    })
+
+    return usuario;
+
   }
 }
 
