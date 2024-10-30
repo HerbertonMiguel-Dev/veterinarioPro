3c541eb036bde1ba0171722cef9bd291de889950
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-10-30 18:51:49 -0300

    adicionado rota para atualização de usuário autenticado

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { Router, Request, Response } from 'express'
 import { CreateUserController } from './controllers/usuario/CreateUserController';
 import { AuthUserController } from './controllers/usuario/AuthUserController'
 import { DetailUserController } from './controllers/usuario/DetailUserController'
+import { UpdateUserController } from './controllers/usuario/UpdateUserController'
 
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
@@ -12,5 +13,6 @@ const router = Router();
 router.post('/usuarios', new CreateUserController().handle.bind(new CreateUserController()));
 router.post('/sessao', new AuthUserController().handle)
 router.get('/me', isAuthenticated, new DetailUserController().handle)
+router.put('/usuarios', isAuthenticated, new UpdateUserController().handle)
 
 export { router };
