652153dd29e8bf9fe40b0fa763cef557b8177357
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-10-31 18:13:43 -0300

    implementado serviço para verificar status da assinatura do usuário

---
+++ src/services/consulta/CheckSubscriptionService.ts
@@ -0,0 +1,29 @@
+import prismaClient from "../../prisma";
+
+interface CheckSubscription{
+  usuario_id: string;
+}
+
+class CheckSubscriptionService{
+  async execute({ usuario_id }: CheckSubscription){
+
+    const status = await prismaClient.usuario.findFirst({
+      where:{
+        id: usuario_id
+      },
+      select:{
+        assinaturas:{
+          select:{
+            id: true,
+            status: true,
+          }
+        }
+      }
+    })
+
+    return status;
+
+  }
+}
+
+export { CheckSubscriptionService }
